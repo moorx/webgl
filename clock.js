d7809cb7f2c694150504b2e84f2e7e82d58f9c02
Christoph Lang <mail.clang@gmail.com>
2011-05-04 02:00:45 +0200

    using "self" in clock wasn't necessary

--- clock.js
+++ clock.js
@@ -1,16 +1,15 @@
 function Clock() {
-  var self = this;
-  self.start = Date.now();
-  self.last = Date.now();
-  self.delta = Date.now() - self.last;
-  self.time = Date.now();
-  self.frame = 0;
+  this.start = Date.now();
+  this.last = Date.now();
+  this.delta = Date.now() - this.last;
+  this.time = Date.now();
+  this.frame = 0;
 
   this.update = function() {
     var now = Date.now();
-    self.delta = now - self.last;
-    self.time = now - self.start;
-    self.last = now;
-    self.frame++;
+    this.delta = now - this.last;
+    this.time = now - this.start;
+    this.last = now;
+    this.frame++;
   };
 }
